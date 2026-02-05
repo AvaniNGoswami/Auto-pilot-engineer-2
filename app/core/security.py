3e7a569d47bb814e8a45b68a8b3243f8f09a7dbe
Avani N. Goswami <avanicasio@gmail.com>
2026-02-05 15:20:18 +0530

    updated logic of authentication

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Optional
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+# from fastapi.security import OAuth2PasswordBearer
 from app.core.config import SECRET_KEY, ACCESS_TOKEN_EXPIRE, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
 import jwt
 from jwt import PyJWTError
@@ -11,8 +11,10 @@ from sqlalchemy.orm import Session
 from app.db.database import engine
 from uuid import uuid4
 
-oath2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+# oath2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
+security = HTTPBearer()
 
 def create_access_token(user_id:str, expires_delta:Optional[timedelta]=None) -> str:
     jti = str(uuid4())
@@ -55,25 +57,53 @@ def decode_access_token(token:str) -> dict:
     except:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="invalid or ixpired token", headers={"WWW-Authentication":"Bearer"})
 
-
-def get_current_user(token:str=Depends(oath2_schema)) -> User:
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+) -> User:
+    token = credentials.credentials
     payload = decode_access_token(token)
+
     user_id = payload["sub"]
     jti = payload["jti"]
 
-    with Session(engine) as session: 
-        auth_session = session.query(AuthSession).filter_by(token_id=jti, is_active=True).first()
+    with Session(engine) as session:
+        auth_session = session.query(AuthSession).filter_by(
+            token_id=jti, is_active=True
+        ).first()
+
         if not auth_session:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="session expired or logged out"
             )
+
         user = session.get(User, user_id)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="user not found"
             )
-        
+
         return user
+
+# def get_current_user(token:str=Depends(oath2_schema)) -> User:
+#     payload = decode_access_token(token)
+#     user_id = payload["sub"]
+#     jti = payload["jti"]
+
+#     with Session(engine) as session: 
+#         auth_session = session.query(AuthSession).filter_by(token_id=jti, is_active=True).first()
+#         if not auth_session:
+#             raise HTTPException(
+#                 status_code=status.HTTP_401_UNAUTHORIZED,
+#                 detail="session expired or logged out"
+#             )
+#         user = session.get(User, user_id)
+#         if not user:
+#             raise HTTPException(
+#                 status_code=status.HTTP_401_UNAUTHORIZED,
+#                 detail="user not found"
+#             )
+        
+#         return user
                                                             
