f5c0d568ce43b50688ac9f64410366426e439357
Avani N. Goswami <avanicasio@gmail.com>
2026-02-05 11:26:15 +0530

    changed db url so engine reads railway db url

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,7 @@
+import os
 from sqlalchemy import create_engine
 
-DATABASEURL =  "postgresql+psycopg2://postgres:admin123@localhost:5432/autopilot_engineer"
+# DATABASEURL =  "postgresql+psycopg2://postgres:admin123@localhost:5432/autopilot_engineer"
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
-engine = create_engine(DATABASEURL, echo=True)
-
+engine = create_engine(DATABASE_URL, echo=True,connect_args={"options": "-c timezone=utc"})
