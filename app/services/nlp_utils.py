664db2bad30f4bac0b9e4b391d1a23ef21770a57
Avani N. Goswami <avanicasio@gmail.com>
2026-02-05 16:57:37 +0530

    updated logic of explaination

--- app/services/nlp_utils.py
+++ app/services/nlp_utils.py
@@ -24,47 +24,75 @@
 
 
 
-import numpy as np
-from app.services.embed_service import embed_text
+# import numpy as np
+# from app.services.embed_service import embed_text
+# import os
 
 
-def embed_message(messages):
-    """
-    Get embeddings for list of messages using HF API
-    """
-    return embed_text(messages)
+# def embed_message(messages):
+#     """
+#     Get embeddings for list of messages using HF API
+#     """
+#     return embed_text(messages)
 
+
+# # def get_most_relevant_message(suggestion, messages):
+
+# #     # Get embedding for suggestion
+# #     sugg_embed = np.array(embed_text([suggestion]))
+
+# #     # Get embeddings for messages
+# #     msg_embed = np.array(embed_text(messages))
+
+# #     # Cosine-like similarity using dot product
+# #     similarities = np.dot(msg_embed, sugg_embed.T).flatten()
+
+# #     idx = np.argmax(similarities)
+
+# #     return messages[idx]
 
 # def get_most_relevant_message(suggestion, messages):
+#     sugg_embed = np.array(embed_text(suggestion))
 
-#     # Get embedding for suggestion
-#     sugg_embed = np.array(embed_text([suggestion]))
+#     msg_embeds = np.array([
+#         embed_text(msg) for msg in messages
+#     ])
 
-#     # Get embeddings for messages
-#     msg_embed = np.array(embed_text(messages))
-
-#     # Cosine-like similarity using dot product
-#     similarities = np.dot(msg_embed, sugg_embed.T).flatten()
+#     similarities = np.dot(msg_embeds, sugg_embed)
 
 #     idx = np.argmax(similarities)
-
 #     return messages[idx]
 
-def get_most_relevant_message(suggestion, messages):
-    sugg_embed = np.array(embed_text(suggestion))
 
-    msg_embeds = np.array([
-        embed_text(msg) for msg in messages
-    ])
 
-    similarities = np.dot(msg_embeds, sugg_embed)
+# def generate_natural_explanation(suggestion, context_msg=None):
 
-    idx = np.argmax(similarities)
-    return messages[idx]
+#     # You are not using any LLM yet
+#     return f"{suggestion} consider this based on recent activity : {context_msg or 'N/A'}"
 
 
 
-def generate_natural_explanation(suggestion, context_msg=None):
 
-    # You are not using any LLM yet
-    return f"{suggestion} consider this based on recent activity : {context_msg or 'N/A'}"
+# app/services/embed_service.py
+
+import os
+from huggingface_hub import InferenceClient
+from functools import lru_cache
+
+print("HF_TOKEN at import time:", os.getenv("HF_TOKEN"))
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+if not HF_TOKEN:
+    raise RuntimeError("HF_TOKEN is missing at runtime")
+
+client = InferenceClient(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    token=HF_TOKEN
+)
+
+@lru_cache(maxsize=200)
+def embed_text(texts):
+    if isinstance(texts, str):
+        texts = [texts]
+    return client.feature_extraction(texts)
