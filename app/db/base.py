7136687cd51f6ee878c71b3a289c8c952b5e8673
Avani N. Goswami <avanicasio@gmail.com>
2026-02-05 11:53:40 +0530

    added all tables in base

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,13 @@
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from app.models.activity_text import ActivityText
+from app.models.feedback import Feedback
+from app.models.githubaccount import GitHubAccount
+from app.models.activity import Activity
+from app.models.auth_session import AuthSession
+from app.models.user import User
+from app.models.in_out import In_Out
+from app.models.features import Features
+
